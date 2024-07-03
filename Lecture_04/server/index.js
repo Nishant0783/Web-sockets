1a1f8ef03ada183dbc9348b09f66d336233f0a2e
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-07-03 23:38:23 +0530

    Lecture_04 do activity detection

---
+++ Lecture_04/server/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+
+const app = express()
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log("Listening to port: ", PORT)
+})
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`)
+
+    // Upon connection - only to user
+    socket.emit('message', "Welcome to chat app!") // goes only to user
+
+    // Upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`) // goes to everyone except the user
+
+    // Listening for a message event
+    socket.on('message', data => {
+        console.log("data is: ", data)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    // When users disconnects
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`) // goes to everyone except the user
+    })
+
+    // Listen for activity
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
+
+    })
+})
